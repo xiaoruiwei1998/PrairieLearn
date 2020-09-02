f128c2abef34d5781d9ad37026fe1b0fa55aa06e
ZhiningQiu <aaronpapa@ZhiningQius-MacBook-Pro.local>
2020-09-02 19:00:42 +0800

    improvements based on variable declaration and function

--- pages/studentAssessmentHomework/studentAssessmentHomework.sql
+++ pages/studentAssessmentHomework/studentAssessmentHomework.sql
@@ -3,12 +3,13 @@ SELECT
     ai.*
 FROM
     assessment_instances AS ai
-    LEFT JOIN groups AS g ON (g.id = ai.group_id AND g.deleted_at IS NULL)
-    LEFT JOIN group_users AS gu ON (gu.group_id = g.id)
+    LEFT JOIN groups AS gr ON gr.id = ai.group_id
+    LEFT JOIN group_users AS gu ON (gu.group_id = gr.id)
 WHERE
     ai.assessment_id = $assessment_id
     AND ai.number = 1
-    AND ((gu.user_id = $user_id) OR (ai.user_id = $user_id));
+    AND ((gu.user_id = $user_id) OR (ai.user_id = $user_id))
+    AND gr.deleted_at IS NULL;
 
 -- BLOCK get_config_info
 SELECT
