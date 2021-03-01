0fc42bdca4615c0b6b3dd6f5346dbd9ba2bb5169
David Gene <david@astrosticks.com>
2021-03-01 14:43:41 -0600

    untested defaults change to fix matt\'s deployment issue

--- migrations/227_force_sequential_questions__add.sql
+++ migrations/227_force_sequential_questions__add.sql
@@ -3,4 +3,4 @@ ALTER TABLE assessments ADD COLUMN advance_score_perc DOUBLE PRECISION;
 ALTER TABLE zones ADD COLUMN advance_score_perc DOUBLE PRECISION;
 
 ALTER TABLE assessment_questions ADD COLUMN advance_score_perc DOUBLE PRECISION;
-ALTER TABLE assessment_questions ADD COLUMN effective_advance_score_perc DOUBLE PRECISION;
+ALTER TABLE assessment_questions ADD COLUMN effective_advance_score_perc DOUBLE PRECISION DEFAULT 0 NOT NULL;
