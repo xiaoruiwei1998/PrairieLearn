63b1c2c1cf919d825d3212893c142cf99dcfbd27
肖睿威 <ruiweixiao98@gmail.com>
2022-07-23 10:20:52 -0400

    remove sp16

---
+++ testCourse/serverFilesCourse/MultiFileServer.js
@@ -0,0 +1,18 @@
+define([], function () {
+  var server = {};
+
+  server.getData = function (vid, options, questionDir) {
+    var params = {
+      requiredFiles: options.requiredFiles || [],
+    };
+
+    var trueAnswer = {};
+
+    return {
+      params: params,
+      trueAnswer: trueAnswer,
+    };
+  };
+
+  return server;
+});
